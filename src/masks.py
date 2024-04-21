86f56d20d81fa3c831189bf9341cd673d80fd8a9
Konstantin Antonov <kostaantonov59@mail.ru>
2024-04-21 13:47:53 +0300

    erge conflict resolved

--- src/masks.py
+++ src/masks.py
@@ -11,28 +11,3 @@ def mask_account_number(account_number: str) -> str:
     """
     digits = "".join([i for i in account_number if i.isdigit()])
     return f"**{digits[-4:]}"
-
-
-def mask_info(info: str) -> str:
-    """
-    Определяет тип карты и счета
-    """
-    parts = info.split(" ", 1)
-    if len(parts) != 2:
-        return "Invalid info"
-
-    if "Счет" in parts[0]:
-        masked_number = mask_account_number(parts[1])
-    else:
-        masked_number = mask_card_number(parts[1])
-
-    return f"{parts[0]} {masked_number}"
-
-
-card_info = input("Введите тип карты и номер карты: ")
-masked_card_info = mask_info(card_info)
-print(masked_card_info)
-
-account_info = input("Введите 'Счет' и номер счета: ")
-masked_account_info = mask_info(account_info)
-print(masked_account_info)
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,23 @@
+def mask_card_or_account(info: str) -> str:
+    """
+    Определяет тип карты или счета и маскирует числа
+    """
+    name, number = info.rsplit(' ', 1)
+
+    if "Счет" in name:
+        masked_number = "**" + number[-4:]
+    else:
+        number = number.replace(' ', '')
+        masked_number = number[:6] + '*' * 6 + number[12:]
+        masked_number = ' '.join([masked_number[i:i + 4] for i in range(0, len(masked_number), 4)])
+
+    return name + ' ' + masked_number
+
+
 def reformat_date(date_string: str) -> str:
+    """
+    Возвращает строку с датой в виде 11.07.2018
+    """
     year, month, day = date_string.split("T")[0].split("-")
     return f"{day}.{month}.{year}"
 
@@ -6,3 +25,7 @@ def reformat_date(date_string: str) -> str:
 date_string = "2018-07-11T02:26:18.671407"
 reformatted_date = reformat_date(date_string)
 print(reformatted_date)
+
+info = input("Введите тип карты и номер карты или 'Счет' и номер счета: ")
+masked_info = mask_card_or_account(info)
+print(masked_info)
