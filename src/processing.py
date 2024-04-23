ddc715403eb8036b61ac4f4f4501965f0ea6b0d1
Konstantin Antonov <kostaantonov59@mail.ru>
2024-04-23 22:23:54 +0300

    commit

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import Dict, List
 
 
 def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
@@ -7,7 +7,7 @@ def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
     filtered_data = []
     for item in data:
-        if item.get('state') == state:
+        if item.get("state") == state:
             filtered_data.append(item)
     return filtered_data
 
@@ -19,13 +19,15 @@ def sort_by_date(data: List[Dict], order: str = "desc") -> List[Dict]:
     if order not in ["desc", "asc"]:
         raise ValueError("order должен быть 'desc' или 'asc'")
 
-    return sorted(data, key=lambda item: item['date'], reverse=(order == "desc"))
+    return sorted(data, key=lambda item: item["date"], reverse=(order == "desc"))
 
 
-data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 sorted_data = sort_by_date(data)
 print(sorted_data)
--- src/widget.py
+++ src/widget.py
@@ -2,16 +2,18 @@ def mask_card_or_account(info: str) -> str:
     """
     Определяет тип карты или счета и маскирует числа
     """
-    name, number = info.rsplit(' ', 1)
+    name, number = info.rsplit(" ", 1)
 
     if "Счет" in name:
         masked_number = "**" + number[-4:]
     else:
-        number = number.replace(' ', '')
-        masked_number = number[:6] + '*' * 6 + number[12:]
-        masked_number = ' '.join([masked_number[i:i + 4] for i in range(0, len(masked_number), 4)])
+        number = number.replace(" ", "")
+        masked_number = number[:6] + "*" * 6 + number[12:]
+        masked_number = " ".join(
+            [masked_number[i : i + 4] for i in range(0, len(masked_number), 4)]
+        )
 
-    return name + ' ' + masked_number
+    return name + " " + masked_number
 
 
 def reformat_date(date_string: str) -> str:
